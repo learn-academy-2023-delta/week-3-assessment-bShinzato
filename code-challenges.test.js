a172d3e840954867a0f71830203e897ec66e1dbb
bShinzato <bshinzato@outlook.com>
2023-05-30 00:52:14 -0700

    Finished week 3.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,19 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("fibSeq", () => {
+  it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
+    const fibLength1 = 6
+    // Expected output: [1, 1, 2, 3, 5, 8]
+    const fibLength2 = 10
+    // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(fibSeq(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibSeq(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
+
+// ReferenceError: fibSeq is not defined
+
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
@@ -22,12 +35,93 @@ const fibLength2 = 10
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+const fibSeq = (fibSeqLen) => {
+  let newArray = []
+  let x = 1
+  let y = 1
+  let z = null
+  newArray.push(x)
+  newArray.push(y)
+  for (let i = 2; i < fibSeqLen; i++) {
+      z = x + y
+      x = y
+      y = z
+      newArray.push(z)
+  } return newArray
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+//Pseudo Code: For Above
+// 1. Create a function named fibSeq.
+    // 1a. Has one parameter named fibSeqLen.
+// 2. Declare 4 new variables.
+    // 2a. First; newArray = []; Since output is an array.
+    // 2b. Second; x = 1, Third; y = 1
+    // 2c. Fourth; z = null; intentional absence of a value.
+    // 2d. Use built in method .push for both x and y into the newArray. This will give us the expected output for the first two elements.
+// 3. Create a for loop, starting at 2, ending at our parameter -since our parameter datatype is a number-, listing one by one.
+// 4. Within the for loop;
+    // 4a. z = x + y; x and y are defined above outside the for loop at the start.
+              // x and y will change depending on our value(s) or sum of 4b and 4c.
+    // 4b. x = y; stating x = y.
+              // This value will change depending on our value from 4c. 
+    // 4c. y = z; stating y = z
+              // This value will change depending on our sum from 4a.
+    // 4d. Use .push to push the new value of z into the array.
+// 5. return newArray outside of the for loop.
+
+// Pseudo code: for below refactored from above.
+// 1. Create a function named fibSeq. Has one parameter named fibSeqLen.
+// 2. Declare a new variable named newArr equal to an array of [1, 1]. These Fixed values are in place for our x and y.
+// 3. Create a for loop with build in methods for starting and ending for i. 
+    //3a. Starting newArr.length, and ending will be our parameter.
+    //3b. Execute the action of the for loop for i. 
+    //3c. Attach bracket notation to newArr[i]. Have it equal a arithmetic equation. newArr[i-1] + newArr[i-2]. 
+// 4.  Return newArr outside of the loop
+
+// const fibSeq = (fibSeqLen) => {
+//   let newArr = [1, 1]
+//   for (let i = newArr.length; i < fibSeqLen; i++) {
+//       newArr[i] = newArr[i-1] + newArr[i-2]
+//   } return newArr
+// }
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("leastToGreatest", () => {
+  it("returns an array of the values sorted from least to greatest", () => {
+    const studyMinutesWeek1 = {
+      sunday: 90,
+      monday: 30,
+      tuesday: 20,
+      wednesday: 15,
+      thursday: 30,
+      friday: 15,
+      saturday: 60
+    }
+    // Expected output: [15, 15, 20, 30, 30, 60, 90]
+    const studyMinutesWeek2 = {
+      sunday: 100,
+      monday: 10,
+      tuesday: 45,
+      wednesday: 60,
+      thursday: 20,
+      friday: 15,
+      saturday: 65
+    }
+    // Expected output: [10, 15, 20, 45, 60, 65, 100]
+    expect(leastToGreatest(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(leastToGreatest(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+})
+
+//   ReferenceError: leastToGreatest is not defined
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -53,11 +147,37 @@ const studyMinutesWeek2 = {
 
 // b) Create the function that makes the test pass.
 
+const leastToGreatest = (minutes) => {
+  return Object.values(minutes).sort((a, b) => a - b)
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
 // Pseudo code:
+// 1. Create a function called leastToGreatest. Has one parameter called minutes.
+// 2. Use static Method Object.values(minutes) with our parameter minutes. To call upon the values, not the keys of the object.
+      // 2a. Attach bulit in method that takes a function as an argument, .sort that takes an argument of parameters (a,b) that minus a from b to list least to greatest.
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("sumOf", () => {
+  it("returns an array of the accumulating sum. An empty array should return an empty array", () => {
+    const accountTransactions1 = [100, -17, -23, -9]
+    // Expected output: [100, 83, 60, 51]
+    const accountTransactions2 = [250, -89, 100, -96]
+    // Expected output: [250, 161, 261, 165]
+    const accountTransactions3 = []
+// Expected output: []
+    expect(sumOf(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(sumOf(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(sumOf(accountTransactions3)).toEqual([])
+  })
+})
+
+// ReferenceError: sumOf is not defined
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -70,4 +190,23 @@ const accountTransactions3 = []
 
 // b) Create the function that makes the test pass.
 
+const sumOf = (addition) => {
+  let placeHolder = 0
+ for(let i=0; i<addition.length;i++) {
+     placeHolder = addition[i] + placeHolder
+     addition[i] = placeHolder
+ } return addition
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
 // Pseudo code:
+// 1. Create a function called sumOf, has 1 parameter called addition.
+      // 1a. Declare a new variable named placeHolder equal to 0.
+// 2. Create a for loop to iterate over each index.
+      // 2a. PlaceHolder is within our local scope.
+      // 2b. Have placeHolder equal to our parameter with backet notation to signal index [i], arithmetic operator + to add placeHolder.
+      // 2c. Have addition[i] = placeHolder, so it can iterate each index within the loop to add the last index value to the current one.
+// 3. Return addition outside of the for loop.
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX helps JavaScript translate into HTML. The differences between the two is that JSX uses camelCases to call upon components from javascript.
 
-Researched answer:
+Researched answer: JSX stands for JavaScript XML or Javascript Syntax Extension and it makes it easier to write react applications. As it is a markdown syntax extension to javascript. Also, it behaves very similarly to HTML which a couple of differences. Uses syntatical sugar for react, which makes pieces of code "sweeter". In addition, two syntax difference between JSX and HTML is that JSX has reserved keywords for class and for unlike HTML. So to define class names and for attributes use camelCasing; className and HTMLFor.
 
-1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
+2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager that allows people to share code with each other. The node Modules are modified when the command yarn is ran within the terminal.
 
-Researched answer:
+Researched answer: Yarn is a package manager that manages all dependancy files. Which leads to to package manager will install dependencies, manage the dependencies, and supply us terminal commands to execute tests. The file that is modified is the package.json file, and installs node modules with their dependencies. 
 
 3. What is a React component?
 
-Your answer:
+Your answer: A react component is a function that is written in javascript that can be passed to JSX land in between the react fragments.
 
-Researched answer:
+Researched answer: A React Component is a specific type of function that can contain JavaScript logic and returns a markup for the browser to render. In addition, they are independent and reusable bits of code and returns to HTML. It can be either state or stateless, which refers to it can be either a logic component (holds state) or display component (stateless).
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: The difference between state values and props is that state values has two parameters and is a react hook. One that holds the data, and one that changes the data. Props on the other hand, refer to the state values and can be called upon with curly braces within any file.
 
-Researched answer:
+Researched answer: A state value is a special variable that stores data that determines how components renders and behaves. By using the React Hook which is a built-in method, the React Hook useState() can define the variable with an inital value and provide methods to change that value. In contrast, props is short for properties, and is the communication between reusable components in react i.e React Hooks. It passes data from one component down to a nested component, and the data is usually comes from a value of a State. In comparison, a state value holds information and a prop calls upon it and passes it down to components tasked to deal with user interaction of that information from state.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM stands for Domestic Object Model, and it is the visual respresentation of the website. DOM can range from visual to events.
 
-Researched answer:
+Researched answer: DOM is a visual representation of code. When looking at a website in the browser you are looking at a DOM. When there is changes from the user or broswer i.e clicks, typing, etc. these are DOM events, and only updating that particular instance or element.
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: A div tag can house a lot of content within it, and a span tag is usually for a header or title.
 
-Researched answer:
+Researched answer: The span tag is used on an inline element, and a div tag is used for a block element. You can have a span within a div. An inline level element does not form new blocks of content. In contrast, a block element is formatted visually as blocks i.e paragraphs.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Is referred to OOP, it was created to help developers deal with complexity and size of software systems. Instead of one big blob of dependencies, section off areas that perform code procedures so the program can interact on small parts instead of the one big blob.
 
-2. Ruby:
+2. Ruby: Is an open source OOP that has simplicity both for writing and reading code. It is a general-purpose, dynamically-typed, server-side scripting language. Has many built in methods for practical use.
 
-3. Implicit return:
+3. Implicit return: Unlike Javascripts; every function needs a return. In contrast, ruby has an implicit return which if there is no return specified the last value of the last line of the block of code is automatically returned.
 
-4. Ruby blocks:
+4. Ruby blocks: Is an anonymous function that is passed to a method to determine the outcome, and is defined with do and end. Very different from Javascript syntax, looks more simple. 
 
-5. Ruby hashes:
+5. Ruby hashes: A hash is a collection of unique keys and their values. It is different from an array/object. Instead of the values being an index, it is referenced by their unique key. Also falls under enumerable which means you can use iterable methods on a hash.
